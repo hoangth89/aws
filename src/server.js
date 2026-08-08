5011bee4e0e577176147e23d36b4cc6870dd1fc5
HoangTH <hoangth89@gmail.com>
2026-08-08 15:26:41 +0700

    fix redis

--- src/server.js
+++ src/server.js
@@ -4,7 +4,12 @@ const redis = require("./config/redis");
 const PORT = process.env.PORT || 3000;
 
 (async () => {
-    await redis.connect();
+    try {
+        await redis.connect();
+        console.log("Connected to Redis");
+    } catch (err) {
+        console.error("Error connecting to Redis:", err);
+    }
 
     app.listen(PORT, () => {
         console.log(`Server running on ${PORT}`);
