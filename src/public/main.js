18e4c6f01860814986b3c6e672448655b8ec1e6c
HoangTH <hoangth89@gmail.com>
2026-08-19 10:36:30 +0700

    cognito

--- src/public/main.js
+++ src/public/main.js
@@ -3,7 +3,7 @@ import { UserManager } from "oidc-client-ts";
 const cognitoAuthConfig = {
     authority: "https://cognito-idp.ap-southeast-2.amazonaws.com/ap-southeast-2_gyzClmGwX",
     client_id: "21b6jjs7r2effrlrke49ud14ao",
-    redirect_uri: "https://aws.dict.io.vn/",
+    redirect_uri: "https://aws.dict.io.vn/redirect",
     response_type: "code",
     scope: "phone openid email"
 };
